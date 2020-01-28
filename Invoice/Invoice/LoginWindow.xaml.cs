4ee528839339b03fbddabf32d4022b71ef8425e4
ceftyc02 <ceftyc02@CS.UCY.AC.CY>
2020-01-28 18:31:16 +0200

    Renamed MainWindow to LoginWindow, added Page1

---
+++ Invoice/Invoice/LoginWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Invoice
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        private static String pass = "pass";
+        private static String user = "admin";
+
+        public LoginWindow()
+        {
+            InitializeComponent();
+            login_button.Click += myButton_Click;
+        }
+        void myButton_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 wnd = new Window1();
+            Console.WriteLine();
+            wnd.Show();
+            if (password.Password == pass && username.Text == user)
+            {
+                login_button.Content = "Succeed";
+            }
+        }
+
+
+    }
+}
