3b3df4f1e789f0353e5dfb410fe4b63215733e9f
ceftyc02 <ceftyc02@cs.ucy.ac.cy>
2020-01-29 13:17:32 +0200

    Main menu

--- Invoice/Invoice/MainWindow.xaml.cs
+++ Invoice/Invoice/MainWindow.xaml.cs
@@ -19,9 +19,77 @@ namespace Invoice
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Page m_home;
+        private Page m_invoice = new Page1();
+        private Page m_receipt;
+        private Page m_creditNote;
+        private Page m_statement;
+        private Page m_product;
+        private Page m_customers;
+        private Page m_order;
+        private Page m_offer;
+        private Page m_expenses;
+        private Page m_settings;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void BtnHome_Click(object sender, RoutedEventArgs e)
+        {
+            pageTitle.Text = "Home";
+        }
+
+        private void BtnInvoice_Click(object sender, RoutedEventArgs e)
+        {
+            pageTitle.Text = "Invoice";
+            MainPage.Content = m_invoice;
+        }
+
+        private void BtnReceipt_Click(object sender, RoutedEventArgs e)
+        {
+            pageTitle.Text = "Receipt";
+        }
+
+        private void BtnCreditNote_Click(object sender, RoutedEventArgs e)
+        {
+            pageTitle.Text = "Credit Note";
+        }
+
+        private void BtnStatement_Click(object sender, RoutedEventArgs e)
+        {
+            pageTitle.Text = "Statement";
+        }
+
+        private void BtnProduct_Click(object sender, RoutedEventArgs e)
+        {
+            pageTitle.Text = "Product";
+        }
+
+        private void BtnCustomers_Click(object sender, RoutedEventArgs e)
+        {
+            pageTitle.Text = "Customers";
+        }
+
+        private void BtnOrder_Click(object sender, RoutedEventArgs e)
+        {
+            pageTitle.Text = "Order";
+        }
+
+        private void BtnOffer_Click(object sender, RoutedEventArgs e)
+        {
+            pageTitle.Text = "Offer";
+        }
+
+        private void BtnExpenses_Click(object sender, RoutedEventArgs e)
+        {
+            pageTitle.Text = "Expenses";
+        }
+
+        private void BtnSettings_Click(object sender, RoutedEventArgs e)
+        {
+            pageTitle.Text = "Settings";
         }
     }
 }
