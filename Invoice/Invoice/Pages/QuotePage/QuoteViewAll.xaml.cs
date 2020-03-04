9464a1078ee1aa8452022b2a793b2766f97b53df
ceftyc02 <ceftyc02@B121WS28>
2020-03-04 13:46:52 +0200

    #19 Basic Files

---
+++ Invoice/Invoice/Pages/QuotePage/QuoteViewAll.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InvoiceX.Pages.QuotePage
+{
+    /// <summary>
+    /// Interaction logic for QuoteViewAll.xaml
+    /// </summary>
+    public partial class QuoteViewAll : Page
+    {
+        public QuoteViewAll()
+        {
+            InitializeComponent();
+        }
+    }
+}
